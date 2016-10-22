13b595929a0657488eed4d060e0f2ea75058243e
Manuel Rauber <manuel.rauber@thinktecture.com>
2016-10-22 16:20:08 +0200

    Demo 2

---
+++ demo-2/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var frames = {
+    'frame-1': document.querySelector('#frame-1'),
+    'frame-2': document.querySelector('#frame-2'),
+    'frame-3': document.querySelector('#frame-3')
+};
+
+function onMessage(event) {
+    // For production always check the full url here.
+    // For this demo, we just check the port
+    if (event.origin.indexOf(':8080') === -1) {
+        return;
+    }
+
+    if (event.data.type === 'resize') {
+        handleResizeMessage(event.data);
+    }
+
+    if (event.data.type === 'navigate') {
+        handleNavigation(event.data);
+    }
+}
+
+function handleResizeMessage(event) {
+    var frame = frames[event.from];
+
+    if (frame) {
+        frame.style.height = event.data + 'px';
+    }
+}
+
+function handleNavigation(event) {
+    frames['frame-3'].contentWindow.postMessage(event, '*');
+}
+
+window.addEventListener('message', onMessage);
